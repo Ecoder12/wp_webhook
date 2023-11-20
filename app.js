28c7d891784845fd275fe0baeaf09a65e9ee24ed
Nikhil Gupta <support@naxtre.com>
2023-11-20 13:25:50 +0530

    added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const sql = require('mssql');
 const ejs = require('ejs');
-
+const cors = require('cors');
 
 const app = express();
 
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
+app.use(cors());
 app.get('/', (req, res) => {
   res.render('form');
 });
